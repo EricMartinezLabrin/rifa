d454b49fa573b2397093d2a8f25e9a6f2816f2f7
EricMartinezLabrin <98716898+EricMartinezLabrin@users.noreply.github.com>
2024-04-16 16:26:03 -0600

    Actualizar URL de la API y agregar navegación a la pantalla de inicio

--- App.js
+++ App.js
@@ -2,10 +2,27 @@ import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import MainNavigation from "./src/Navigation/MainNavigation";
+import Gallery from "./src/Screens/Gallery";
 
 export default function App() {
+  const linking = {
+    prefixes: ["https://rifa.fadetechs.com", "rifa://"],
+    config: {
+      screens: {
+        Inicio: "Inicio",
+        Favoritos: "Premios",
+        MyEvents: "Tyc",
+        MyAccount: "Pagar Pendientes",
+        Gallery: "Gallery",
+        GalleryList: "GalleryList",
+        Register: "Register",
+        Stripe: "Stripe",
+      },
+    },
+  };
+
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       <StatusBar style="auto" />
       <MainNavigation />
     </NavigationContainer>
--- src/Screens/Premios.js
+++ src/Screens/Premios.js
@@ -1,66 +1,73 @@
-// import { View, Text, StyleSheet, Pressable, FlatList } from "react-native";
-// import React from "react";
-
-// export default function Premios(props) {
-//   const { navigate } = props.navigation;
-//   const goToGallery = (id) => {
-//     navigate("Gallery", { id: id });
-//   };
-//   return (
-//     <FlatList
-//       data={premios}
-//       keyExtractor={(item) => item.id}
-//       renderItem={({ item }) => (
-//         <View style={styles.container}>
-//           <Text style={styles.title}>{item.id} lugar</Text>
-//           <Text style={styles.subtitle}>{item.nombre}</Text>
-//           <Text style={styles.detail}>
-//             Premios Disponibles: {item.cantidad}
-//           </Text>
-//           <Text style={styles.detail}>{item.detail}</Text>
-//           <Pressable onPress={() => goToGallery(item.id)}>
-//             <Text style={styles.pictureLink}>Ver Fotos...</Text>
-//           </Pressable>
-//         </View>
-//       )}
-//     ></FlatList>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     margin: 20,
-//     maxWidth: 700,
-//   },
-//   title: {
-//     fontSize: 24,
-//     fontWeight: "bold",
-//     marginBottom: 20,
-//     textAlign: "center",
-//   },
-//   subtitle: {
-//     fontSize: 18,
-//     fontWeight: "bold",
-//   },
-//   detail: {
-//     fontSize: 18,
-//     marginBottom: 10,
-//   },
-//   pictureLink: {
-//     fontSize: 18,
-//     color: "blue",
-//     textDecorationLine: "underline",
-//     marginBottom: 20,
-//   },
-// });
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Pressable, FlatList } from "react-native";
 import React from "react";
 
-export default function Premios() {
+import { getRewardApi } from "../Api/backend";
+
+export default function Premios(props) {
+  const { navigate } = props.navigation;
+  const [premios, setPremios] = React.useState([]);
+
+  React.useEffect(() => {
+    try {
+      const getReward = async () => {
+        const premios = await getRewardApi();
+        setPremios(premios.rewards);
+      };
+      getReward();
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  const goToGallery = (id) => {
+    navigate("Gallery", { id: id });
+  };
+
   return (
-    <View>
-      <Text>Premios</Text>
-    </View>
+    <FlatList
+      data={premios}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <View style={styles.container}>
+          <Text style={styles.title}>{item.id} lugar</Text>
+          <Text style={styles.subtitle}>{item.nombre}</Text>
+          <Text style={styles.detail}>
+            Premios Disponibles: {item.cantidad}
+          </Text>
+          <Text style={styles.detail}>{item.detail}</Text>
+          <Pressable onPress={() => goToGallery(item.id)}>
+            <Text style={styles.pictureLink}>Ver Fotos...</Text>
+          </Pressable>
+        </View>
+      )}
+    ></FlatList>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 20,
+    maxWidth: 700,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  detail: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  pictureLink: {
+    fontSize: 18,
+    color: "blue",
+    textDecorationLine: "underline",
+    marginBottom: 20,
+  },
+});
